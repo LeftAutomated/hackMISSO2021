37daaec9cae756a5ca2fa877ad81814d3c24005e
LeftAutomated <bryantle2021@gmail.com>
2021-11-13 23:19:39 -0600

    Update title

--- src/components/TopBar/loginlogoutButton/LoginButton.tsx
+++ src/components/TopBar/loginlogoutButton/LoginButton.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
-const LoginButton = ()=>{
-    const { loginWithRedirect}:any = useAuth0();
+const LoginButton = () => {
+    const { loginWithRedirect }: any = useAuth0();
     return (
-        <div className ="topbar">
-            <div className = "topbarWrapper">
-                <div className = "topleft">
-                    <span className="logo">Data Visualization</span> 
+        <div className="topbar">
+            <div className="topbarWrapper">
+                <div className="topleft">
+                    <span className="logo">AVAS</span>
                 </div>
-                <div className = "topright">
+                <div className="topright">
                     <div className="logInOutButton">
-                        <button onClick={()=> loginWithRedirect()}>
+                        <button onClick={() => loginWithRedirect()}>
                             Log In
                         </button>
                     </div>
                 </div>
             </div>
-        </div>  
+        </div>
     )
 }
 
